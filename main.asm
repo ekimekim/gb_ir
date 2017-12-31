d69a2e2a4e536256055f9307c8512f285e1bbc7c
Mike Lang <mikelang3000@gmail.com>
2017-12-31 07:58:34 -0800

    Rewrite how sampling is done so it can be every 4 cycles

--- main.asm
+++ main.asm
@@ -14,7 +14,7 @@ SECTION "Sample data", WRAM0, ALIGN[8]
 
 ; Note: aligned
 Samples::
-	ds 256
+	ds 18*20*8
 
 
 SECTION "Main", ROM0
@@ -29,14 +29,12 @@ Start::
 	ld SP, Stack
 
 	; Switch into double-speed mode
-	; TODO do we need to enable interrupts to leave STOP mode?
 	ld A, 1
 	ld [CGBSpeedSwitch], A
 	stop
 
 	; Init graphics tiles
 	call GenerateTiles
-	call InitBGMap
 	call InitBGPalette
 
 	; Intialize IO register variables
@@ -108,40 +106,6 @@ GenerateTiles::
 	ret
 
 
-; set up BG tile flags so that the top 1 and bottom 2 rows, as well as all but the 4 left-most tiles
-; of the 3rd bottom row, use an all-white palette (palette 2), whereas other tiles use palette 1,
-; this gives us 256 tiles of display in just over 14 rows.
-InitBGMap::
-	ld A, 1
-	ld [CGBVRAMBank], A ; switch to bank 1
-	; A still = 1, which is also the value we want for flags - selects palette 1
-	ld HL, TileGrid
-	; D and E track rows and count within a row. We flip between A = 0 and 1 (use palette 0 or 1)
-	; every time B hits zero. It starts at -20 so it flips one row in, then flips again
-	; 256 tiles later.
-	ld B, -20
-	ld D, 18
-.loop_outer
-	ld E, 20
-.loop_inner
-	ld [HL+], A
-	inc B
-	jr nz, .no_flip
-	rra ; bottom bit of A goes into carry
-	ccf ; negate carry
-	rla ; move negated bit back out of carry, so we've flipped bottom bit of A
-.no_flip
-	dec E
-	jr nz, .loop_inner
-	LongAddConst HL, 32-20 ; LongAddConst doesn't clobber A, and i'm lazy
-	dec D
-	jr nz, .loop_outer
-	; switch VRAM bank back before returning
-	xor A
-	ld [CGBVRAMBank], A
-	ret
-
-
 InitBGPalette::
 	ld A, 6 | $80 ; selects the palette 0, 11 color value, and enables autoincrement
 	ld [TileGridPaletteIndex], A
@@ -153,17 +117,39 @@ InitBGPalette::
 
 ; Copy rows of sample data into VRAM for display.
 DisplaySamples::
-	ld HL, TileGrid + 32 ; start at second row
-	ld DE, Samples
-	; Lazy way of handling vblank: Copy in 5 parts.
-	REPT 4
 	call WaitForVBlank
-	call CopyRow
-	call CopyRow
-	call CopyRow
-	ENDR
-	call CopyRow
+	xor A
+	ld [LCDControl], A ; turn off screen
+
+	ld DE, TileGrid
+	ld HL, Samples
+	ld B, 18
+.outer
+	ld C, 20
+.inner
+	push BC
+	ld B, 0
+REPT 8
+	ld A, [HL+] ; A = raw sample, actual sample is inverted and in bit 1
+	rra
+	rra ; rotate until sample bit is in carry
+	rl B ; rotate carry bit back out into B
+ENDR
+	ld A, B
+	cpl ; invert, so 1 is a high reading
+	ld [DE], A
+	inc DE
+	pop BC
+	dec C
+	jr nz, .inner
+	LongAdd D,E, 0,12, D,E
+	dec B
+	jr nz, .outer
+	; Turn on screen, use unsigned tilemap
+	ld A, %10010000
+	ld [LCDControl], A
 	ret
+
 
 WaitForVBlank:
 	; first, wait until we AREN'T in vblank so we can capture the start
@@ -178,18 +164,6 @@ WaitForVBlank:
 	and %00000011
 	cp 1
 	jr nz, .loop2
-	ret
-
-CopyRow:
-	ld B, 20
-.loop
-	ld A, [DE]
-	ld [HL+], A
-	inc E
-	ret z ; early exit if E wraps (which it will on the 15th call)
-	dec B
-	jr nz, .loop
-	LongAdd H,L, 0,(32-20), H,L
 	ret
 
 
@@ -208,49 +182,23 @@ WaitForButton::
 	ret
 
 
-; TakeSamples macros
-
-NopFor: MACRO
-REPT (\1)
-	nop
-ENDR
-ENDM
-
 TakeSamples::
-	ld [StackSave], SP ; for safekeeping. We're going to abuse push as a 16-bit ldi.
-	ld SP, Samples + 256
-	ld HL, CGBInfrared ; more flexibility than ld A, [C] and just as fast
+	ld C, LOW(CGBInfrared)
+	ld HL, Samples
 	; A lot of our normal tricks for speed don't work well here, because samples need to be
 	; a consistent timing apart. so eg. unrolling doesn't help since we are constrained by
 	; the _longest_ time between samples.
-BUDGET EQU 8
 
-	; First 2 bytes is a special case, no cleanup from prev round
-	ld A, [HL]
-	rra ; first time's a special case: just shift once and you're done
-	ld B, A
-	NopFor BUDGET - 4
-REPT 6
-	ld A, [HL]
-	rra
-	rra ; shift A right twice -> sample bit is now in carry flag
-	rl B ; shift carry into B
-	NopFor BUDGET - 6
+	; This is the mother of all unrolls. But it works.
+	; Takes one sample every 4 cycles.
+	; Takes enough samples to fill the screen with info.
+	; Total sample coverage time: 2^-21 * 4 * 18 * 20 * 8 = 2^-16 * 18 * 20 = 45 * 2^-13 ~= 5.5ms
+	; This is a bit of a problem, since we aren't guarenteed to spot the 60Hz pattern.
+	; It can be almost entirely fixed by expanding the display to cover the whole 32x32 tilegrid
+	; and writing some code to scroll (total coverage would then be 15.625ms), but this will
+	; do for now. We can always slow it down if we must!
+REPT 18*20*8
+	ld A, [C]
+	ld [HL+], A
 ENDR
-	ld A, [HL]
-	rra ; first time's a special case: just shift once and you're done
-	ld C, A
-	NopFor BUDGET - 4
-REPT 6
-	ld A, [HL]
-	rra
-	rra ; shift A right twice -> sample bit is now in carry flag
-	rl C ; shift carry into C
-	NopFor BUDGET - 6
-ENDR
-
-	; Second and all subsequent blocks interleaves finally writing prev block
-	; with doing samples
-	ld D, [HL] ; no time to do anything with the sample, just shove it elsewhere for now
-	push BC ; actually writes the last 16 samples
-	NopFor BUDGET - 6
+	ret
